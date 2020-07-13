6c4790aa188b662dbae09415f357a7637364f7b1
barkgnar <45748185+kenzrad@users.noreply.github.com>
2020-07-13 15:09:02 -0400

    remove logger middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import searchReducer from '../features/search/searchSlice';
-import { logger } from 'redux-logger';
+// import { logger } from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import {combineReducers} from 'redux';
 import storage from 'redux-persist/lib/storage';
@@ -18,8 +18,8 @@ const persistedReducer = persistReducer(persistConfig, reducers)
 
 // the default serializableStateInvariant middleware throws an exception from redux-persist (although it performs as it should) - this middleware would not be included in prod environments
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware().concat(logger)
+  reducer: persistedReducer
+  // middleware: getDefaultMiddleware().concat(logger)
 });
 
 export const persistor = persistStore(store);
