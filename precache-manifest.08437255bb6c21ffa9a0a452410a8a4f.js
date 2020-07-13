cdb91ec1f942f625a36f249b4a04cee40f4723bf
barkgnar <45748185+kenzrad@users.noreply.github.com>
2020-07-13 14:54:35 -0400

    Updates

---
+++ precache-manifest.08437255bb6c21ffa9a0a452410a8a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff291b9b58e6cf1cc4cbb8095fa9b1f",
+    "url": "/react-search-news/index.html"
+  },
+  {
+    "revision": "4cbb94f6ecdcb6aab60d",
+    "url": "/react-search-news/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "00715ad268effcb203d1",
+    "url": "/react-search-news/static/js/2.0aabc6f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-search-news/static/js/2.0aabc6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbb94f6ecdcb6aab60d",
+    "url": "/react-search-news/static/js/main.162c38d5.chunk.js"
+  },
+  {
+    "revision": "4d5fa2e68fc13eee3010",
+    "url": "/react-search-news/static/js/runtime-main.6cc62912.js"
+  }
+]);
